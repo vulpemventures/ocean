8265d4547a8569a263eef662d2e19fd39d94197d
Pietralberto Mazza <18440657+altafan@users.noreply.github.com>
2023-01-26 16:31:03 +0100

    [Electrum BC scanner] Reconnect TCP client if connection drops (#48)
    * Reconnect  if connection drops
    
    * Fix after review

--- internal/infrastructure/blockchain-scanner/electrum/utils.go
+++ internal/infrastructure/blockchain-scanner/electrum/utils.go
@@ -2,7 +2,11 @@ package electrum_scanner
 
 import (
 	"crypto/sha256"
+	"crypto/tls"
 	"encoding/hex"
+	"fmt"
+	"net"
+	"strings"
 
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
 )
@@ -12,4 +16,17 @@ func calcScriptHash(script string) string {
 	hashedBuf := sha256.Sum256(buf)
 	hash, _ := chainhash.NewHash(hashedBuf[:])
 	return hash.String()
+}
+
+func getConn(addr string) (net.Conn, error) {
+	split := strings.Split(addr, "://")
+	proto, url := split[0], split[1]
+	switch proto {
+	case "tcp":
+		return net.Dial(proto, url)
+	case "ssl":
+		return tls.Dial("tcp", url, nil)
+	default:
+		return nil, fmt.Errorf("invalid address: unknown prototocol")
+	}
 }
